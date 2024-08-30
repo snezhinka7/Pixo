1701f28b76b131ab34c61ca338c23459e4b82db5
Sofia <crissofia.2003@gmail.com>
2024-08-29 23:02:23 -0300

    diseño de login primer modulo

--- app/src/main/java/com/pixopaginaweb/pixo/MainActivity.kt
+++ app/src/main/java/com/pixopaginaweb/pixo/MainActivity.kt
@@ -11,6 +11,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -18,3 +21,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
